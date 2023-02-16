dca7325a7a763db74538e7b0288d6f2b43f47909
Matti <shalkku@hotmail.com>
2023-02-16 21:42:49 +0200

    General refactoring

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
+const Utils = require('./utils');
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const customRoute = require('./router');
+const { Client } = require('pg')
 
-app.use('/api', customRoute);
+
+
+app.get('/api/getData', function(req,res) {
+   var utils = new Utils();
+   res.send(utils.getData());
+})
+
+
+
+var minutes = 1, the_interval = minutes * 10 * 1000;
+
+setInterval(function() {
+
+   const d = new Date();
+
+   var m = (((d.getMinutes() + 7.5)/15 | 0) * 15) % 60;
+   var h = ((((d.getMinutes()/105) + .5) | 0) + d.getHours()) % 24;
+
+  // console.log('Hours' + h + ' minutes:' + m);
+
+
+   axios.get('https://liikuntaparkki.fi/?controller=ajax&getentriescount=1&locationId=1', {})
+   .then(function(response) {
+       console.log(response.data.entriesTotal);
+   });
+
+
+   /*
+   const client = new Client({
+      user: 'liikuntaparkkidb',
+      host: 'localhost',
+      database: 'postgres',
+      password: 'password',
+      port: 5432,
+   });
+
+   client.connect()
+   client.query('SELECT $1::text as message', ['Hello world!']) // your query string here
+      .then(result => console.log(result.rows)) // your callback here
+      .catch(e => console.error(e.stack)) // your callback here
+      .then(() => client.end())*/
+
+}, the_interval);
+
+
 
 var server = app.listen(8081, function () {
    var host = server.address().address
--- utils.js
+++ utils.js
@@ -3,39 +3,39 @@ const express = require('express');
 
 class Utils {
 
-    getDataMock() {
-        return [
-            {
-                time: '2023-01-01 15:00',
-                visitors: 10
+    data = [
+        {
+            time: '2023-01-01 15:00',
+            visitors: 10
 
-            },
-            {
-                time: '2023-01-01 15:15',
-                visitors: 12
+        },
+        {
+            time: '2023-01-01 15:15',
+            visitors: 12
 
-            },
-            {
-                time: '2023-01-01 15:30',
-                visitors: 15
+        },
+        {
+            time: '2023-01-01 15:30',
+            visitors: 15
 
-            },
-            {
-                time: '2023-01-01 15:45',
-                visitors: 17
+        },
+        {
+            time: '2023-01-01 15:45',
+            visitors: 17
 
-            },
+        },
 
-            {
-                time: '2023-01-01 16:00',
-                visitors: 10
+        {
+            time: '2023-01-01 16:00',
+            visitors: 10
 
-            },
-        ];
-    }
-
+        },
+    ];
 
     getData() {
+
+        return this.data;
+
         axios.get('https://liikuntaparkki.fi/?controller=ajax&getentriescount=1&locationId=1', {})
         .then(function(response) {
             console.log(response.data);

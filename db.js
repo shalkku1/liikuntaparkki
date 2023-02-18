2c6698406a4bc81eb1c91779b73d2cf1bf6e5afc
Matti <shalkku@hotmail.com>
2023-02-18 13:50:31 +0200

    Prod release

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const { Client } = require('pg')
 
-const client = new Client({
+/*const client = new Client({
     user: 'liikuntaparkkidb',
     host: 'localhost',
     database: 'postgres',
     password: 'password',
     port: 5432,
-})
+})*/
+
+const client = new Client({connectionString: process.env.DATABASE_URL})
  
 client.connect()
 

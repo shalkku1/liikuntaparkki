dfa7568f8e5153d6650bbd0293e70594b2850ba2
Konsta <konstakaarineva@gmail.com>
2023-02-20 19:27:20 +0200

    wildcard route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChartPageComponent } from './components/chart-page/chart-page.component'
 
 const routes: Routes = [
-  { path: 'chart-page', component: ChartPageComponent }
+  { path: 'chart-page', component: ChartPageComponent },
+  { path: '**', component: ChartPageComponent }
 ];
 
 @NgModule({
